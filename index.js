89c62d99ac142f05645b88e043e2628d435a9c0b
johnnyteck <drillsjohnny00@gmail.com>
2022-09-28 22:15:58 +0100

    functionality fixed

--- index.js
+++ index.js
@@ -1,48 +1,3 @@
-let deckId = ''
-let cardValue = 0
-fetch(`http://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`) 
-.then(res => res.json()) // parse response as JSON
-  .then(data => {
-    console.log(data)
-    deckId = data.deck_id
-  })
-  .catch(err => {
-      console.log(`error ${err}`)
-  });
-
-function drawCard(player){
-    const url = `http://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`
-    fetch(url)
-    .then(res => res.json()) // parse response as JSON
-    .then(data => {
-        console.log(data)
-        player.src = data.cards[0].image
-        cardValue = data.cards[0].value
-    })
-    .catch(err => {
-        console.log(`error ${err}`)
-    });
-}
-
-
-
-// function getFetch(){
-// //   const choice = document.querySelector('input').value
-
-//   const anotherUrl = `http://deckofcardsapi.com/api/deck/${data.deck_id}/draw/?count=1`
-//   fetch(anotherUrl)
-//   .then(res => res.json()) // parse response as JSON
-//   .then(data => {
-//     console.log(data.deck_id)
-//   })
-//   .catch(err => {
-//       console.log(`error ${err}`)
-//   });
-// }
-
-// getFetch()
-
-
 let player1Score = 0
 let player2Score = 0
 let player1Turn = true
@@ -58,6 +13,23 @@ const rollBtn2 = document.getElementById("rollBtn2")
 const resetBtn = document.getElementById("resetBtn")
 const comment = document.getElementById("text")
 
+let deckId = ''
+let card1Value = 0
+let card2Value = 0
+fetch(`http://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`) 
+.then(res => res.json()) // parse response as JSON
+  .then(data => {
+    console.log(data)
+    deckId = data.deck_id
+  })
+  .catch(err => {
+      console.log(`error ${err}`)
+  });
+
+rollBtn1.addEventListener("click", rollDice1)
+rollBtn2.addEventListener("click", rollDice2)
+
+
 function showResetButton() {
     rollBtn1.style.display = "none"
     rollBtn2.style.display = "none"
@@ -67,52 +39,59 @@ function showResetButton() {
 
 
 /* Hook up a click event listener to the Roll Dice Button. */
-rollBtn1.addEventListener("click", function() {
-    drawCard(player1Card)
+function rollDice1() {
+    const url = `http://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`
+    fetch(url)
+    .then(res => res.json()) // parse response as JSON
+    .then(data => {
+        console.log(data)
 
-    if (player1Turn) {
-        player1Score += cardValue
-        player1Scoreboard.textContent = player1Score
-        // player1Card.textContent = randomNumber1
-        player1Card.classList.remove("active")
-        player2Card.classList.add("active")
-        message.textContent = "Player 2 Turn"
-        player1Turn = !player1Turn
-        if (player1Score >= 20) {
-            message.textContent = "Player 1 Won 🥳"
-            showResetButton()
-        } else if (player2Score >= 20) {
-            message.textContent = "Player 2 Won 🎉"
-            showResetButton()
-        } else { }
-    } else{}
-    
-    
-})
+        if (player1Turn) {
+            player1Card.src = data.cards[0].image
+            card1Value = data.cards[0].value
+
+            player1Score += convertNum(card1Value)
+            player1Scoreboard.textContent = player1Score
+
+            player1Card.classList.remove("active")
+            player2Card.classList.add("active")
+            message.textContent = "Player 2 Turn"
+            player1Turn = !player1Turn
+
+            checkWinner()
+        } else{}
+  })
+    .catch(err => {
+        console.log(`error ${err}`)
+    });   
+}
+
+function rollDice2() {
+    const url = `http://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`
+    fetch(url)
+    .then(res => res.json()) // parse response as JSON
+    .then(data => {
+        console.log(data)
+
+        if(!player1Turn) {
+            player2Card.src = data.cards[0].image
+            card2Value = data.cards[0].value
+
+            player2Score += convertNum(card2Value)
+            player2Scoreboard.textContent = player2Score
+            // player2Card.textContent = randomNumber2
+            player2Card.classList.remove("active")
+            player1Card.classList.add("active")
+            message.textContent = "Player 1 Turn"
+            checkWinner()
+        } else{}
+        player1Turn = true
+  })
+    .catch(err => {
+        console.log(`error ${err}`)
+    });   
+}
  
-
-rollBtn2.addEventListener("click", function() {
-    drawCard(player2Card)
-
-    // const randomNumber2 = Math.floor(Math.random() * 6) + 1
-    // if(!player1Turn) {
-    //     player2Score += randomNumber2
-    //     player2Scoreboard.textContent = player2Score
-    //     player2Dice.textContent = randomNumber2
-    //     player2Dice.classList.remove("active")
-    //     player1Dice.classList.add("active")
-    //     message.textContent = "Player 1 Turn"
-    //     if (player1Score >= 20) {
-    //         message.textContent = "Player 1 Won 🥳"
-    //         showResetButton()
-    //     } else if (player2Score >= 20) {
-    //         message.textContent = "Player 2 Won 🎉"
-    //         showResetButton()
-    //     } else {}
-    // } else{}
-    // player1Turn = true
-})
-
 resetBtn.addEventListener("click", function(){
     reset()
 })
@@ -126,19 +105,44 @@ resetBtn.addEventListener("click", function(){
 // }
 // else {}
 
+// Convert card value to integer
+function convertNum(val) {
+    if (val ===  "ACE") {
+        return 11
+    } else if (val === "KING") {
+        return 14
+    } else if (val === "QUEEN") {
+        return 13
+    } else if (val === "JACK") {
+        return 12
+    } else {
+        return Number(val)
+    }
+}
+
+function checkWinner() {
+    if (player1Score >= 52) {
+        message.textContent = "Player 1 Won 🥳"
+        showResetButton()
+    } else if (player2Score >= 52) {
+        message.textContent = "Player 2 Won 🎉"
+        showResetButton()
+    } else {}
+}
+
 function reset() {
     player1Score = 0
     player2Score = 0
     player1Turn = true
     player1Scoreboard.textContent = 0
     player2Scoreboard.textContent = 0
-    player1Dice.textContent = "-"
-    player2Dice.textContent = "-"
+    player1Card.src = "./images/joker.png"
+    player2Card.src = "./images/joker.png"
     message.textContent = "Player 1 Turn"
     rollBtn1.style.display = "block"
     rollBtn2.style.display = "block"
     resetBtn.style.display = "none"
-    player2Dice.classList.remove("active")
-    player1Dice.classList.add("active")
+    player2Card.classList.remove("active")
+    player1Card.classList.add("active")
 }
 
