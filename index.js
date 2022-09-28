7ebccdf3d854b68429e103ae8fc8a07c54e8786b
johnnyteck <drillsjohnny00@gmail.com>
2022-09-28 19:47:49 +0100

    API fetch

--- index.js
+++ index.js
@@ -1,11 +1,55 @@
-// Create variables for the game state
+let deckId = ''
+let cardValue = 0
+fetch(`http://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`) 
+.then(res => res.json()) // parse response as JSON
+  .then(data => {
+    console.log(data)
+    deckId = data.deck_id
+  })
+  .catch(err => {
+      console.log(`error ${err}`)
+  });
+
+function drawCard(player){
+    const url = `http://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`
+    fetch(url)
+    .then(res => res.json()) // parse response as JSON
+    .then(data => {
+        console.log(data)
+        player.src = data.cards[0].image
+        cardValue = data.cards[0].value
+    })
+    .catch(err => {
+        console.log(`error ${err}`)
+    });
+}
+
+
+
+// function getFetch(){
+// //   const choice = document.querySelector('input').value
+
+//   const anotherUrl = `http://deckofcardsapi.com/api/deck/${data.deck_id}/draw/?count=1`
+//   fetch(anotherUrl)
+//   .then(res => res.json()) // parse response as JSON
+//   .then(data => {
+//     console.log(data.deck_id)
+//   })
+//   .catch(err => {
+//       console.log(`error ${err}`)
+//   });
+// }
+
+// getFetch()
+
+
 let player1Score = 0
 let player2Score = 0
 let player1Turn = true
 
 // Create variables to store references to the necessary DOM nodes
-const player1Dice = document.getElementById("player1Dice")
-const player2Dice = document.getElementById("player2Dice")
+const player1Card = document.getElementById("player1Card")
+const player2Card = document.getElementById("player2Card")
 const player1Scoreboard = document.getElementById("player1Scoreboard")
 const player2Scoreboard = document.getElementById("player2Scoreboard")
 const message = document.getElementById("message")
@@ -24,13 +68,14 @@ function showResetButton() {
 
 /* Hook up a click event listener to the Roll Dice Button. */
 rollBtn1.addEventListener("click", function() {
-    const randomNumber1 = Math.floor(Math.random() * 6) + 1
+    drawCard(player1Card)
+
     if (player1Turn) {
-        player1Score += randomNumber1
+        player1Score += cardValue
         player1Scoreboard.textContent = player1Score
-        player1Dice.textContent = randomNumber1
-        player1Dice.classList.remove("active")
-        player2Dice.classList.add("active")
+        // player1Card.textContent = randomNumber1
+        player1Card.classList.remove("active")
+        player2Card.classList.add("active")
         message.textContent = "Player 2 Turn"
         player1Turn = !player1Turn
         if (player1Score >= 20) {
@@ -47,25 +92,25 @@ rollBtn1.addEventListener("click", function() {
  
 
 rollBtn2.addEventListener("click", function() {
-    const randomNumber2 = Math.floor(Math.random() * 6) + 1
-    if(!player1Turn) {
-        player2Score += randomNumber2
-        player2Scoreboard.textContent = player2Score
-        player2Dice.textContent = randomNumber2
-        player2Dice.classList.remove("active")
-        player1Dice.classList.add("active")
-        message.textContent = "Player 1 Turn"
-        if (player1Score >= 20) {
-            message.textContent = "Player 1 Won 🥳"
-            showResetButton()
-        } else if (player2Score >= 20) {
-            message.textContent = "Player 2 Won 🎉"
-            showResetButton()
-        } else {}
-    } else{}
+    drawCard(player2Card)
 
-    
-    player1Turn = true
+    // const randomNumber2 = Math.floor(Math.random() * 6) + 1
+    // if(!player1Turn) {
+    //     player2Score += randomNumber2
+    //     player2Scoreboard.textContent = player2Score
+    //     player2Dice.textContent = randomNumber2
+    //     player2Dice.classList.remove("active")
+    //     player1Dice.classList.add("active")
+    //     message.textContent = "Player 1 Turn"
+    //     if (player1Score >= 20) {
+    //         message.textContent = "Player 1 Won 🥳"
+    //         showResetButton()
+    //     } else if (player2Score >= 20) {
+    //         message.textContent = "Player 2 Won 🎉"
+    //         showResetButton()
+    //     } else {}
+    // } else{}
+    // player1Turn = true
 })
 
 resetBtn.addEventListener("click", function(){
